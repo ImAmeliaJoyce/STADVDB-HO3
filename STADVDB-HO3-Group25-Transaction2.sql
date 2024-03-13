943766b3dfbd7afd92d20b60df391416206f1e30
Amelia Abenoja <97102877+ImAmeliaJoyce@users.noreply.github.com>
2024-03-14 03:21:51 +0800

    T2 (Read) Summary

--- STADVDB-HO3-Group25-Transaction2.sql
+++ STADVDB-HO3-Group25-Transaction2.sql
@@ -1,54 +1,48 @@
 /*
 	TRANSACTION 2
-	This script file handles the read transactions to be done
-    
-    Isolation levels:
-    - READ UNCOMMITTED
-    - READ COMMITTED
-    - REPEATABLE READ
-    - SERIALIZABLE
+    - Handles the Read(X) Operation
 */
--- Scenario 1 (Dirty Read)
--- FLOW: Transaction 1 will write, then sleep for 10 seconds
--- before COMMIT has executed, execute script below ONLY
 
--- Edit Isolation Level
--- (replace "UNCOMMITTED" with isolation value assigned)
+-- Set Isolation Level always before doing a transaction
 SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED;
+-- SET SESSION TRANSACTION ISOLATION LEVEL READ COMMITTED;
+-- SET SESSION TRANSACTION ISOLATION LEVEL REPEATABLE READ;
+-- SET SESSION TRANSACTION ISOLATION LEVEL SERILIZABLE;
 
+
+-- -------------------------------------
+-- Scenario 1 (Dirty Read)
+-- FLOW: Transaction 1 will write, then after DO SLEEP for 10 seconds
+-- before COMMIT has executed, execute T2 script below ONLY for dirty read
 START TRANSACTION;
 	SELECT *
     FROM datawarehouseho3.accounts
-    WHERE id = 5;
+    WHERE id BETWEEN 1 and 3;
 COMMIT;
 
+
+-- -------------------------------------
 -- Scenario 2 (Non-repeatable Read)
 -- FLOW: Execute this query from Transaction 2 first,
 -- while DO SLEEP(10) is executing, run corresponding query in Transaction 1
 
--- Edit Isolation Level
--- (replace "UNCOMMITTED" with isolation value assigned)
-SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED;
-
 START TRANSACTION;
 	SELECT amount 
     FROM datawarehouseho3.accounts
-    WHERE id = 5;
+    WHERE id = 3;
     
     DO SLEEP(10);
     
     SELECT amount 
     FROM datawarehouseho3.accounts
-    WHERE id = 5;
+    WHERE id = 3;
 COMMIT;
 
+
+-- -------------------------------------
 -- Scenario 3 (Phantom Read)
 -- FLOW: Execute first read, then while waiting, execute corresponding
 -- query in Transaction 1, next query and commit will then be executed
-
--- Edit Isolation Level
--- (replace "UNCOMMITTED" with isolation value assigned)
-SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED;
 
 START TRANSACTION;
 	SELECT *
@@ -61,6 +55,3 @@ START TRANSACTION;
     FROM datawarehouseho3.accounts
     WHERE id BETWEEN 1 and 10;
 COMMIT;
-
-
-
