57e4953a781f1e04e4a8419ff5c476dfb28c2209
Amelia Abenoja <97102877+ImAmeliaJoyce@users.noreply.github.com>
2024-03-14 03:19:31 +0800

    T1 (Write) Non-Repeatable

---
+++ Transaction1-PhantomRead.sql
@@ -0,0 +1,21 @@
+/*
+	TRANSACTION 1
+    - Handles the Write(X) Operation
+*/
+
+-- Set Isolation Level
+SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED;
+-- SET SESSION TRANSACTION ISOLATION LEVEL READ COMMITTED;
+-- SET SESSION TRANSACTION ISOLATION LEVEL REPEATABLE READ;
+-- SET SESSION TRANSACTION ISOLATION LEVEL SERILIZABLE;
+
+
+-- -------------------------------------
+-- Scenario 3 (Phantom Read)
+-- FLOW: Execute this while DO SLEEP(10) is running in Transaction 2
+START TRANSACTION;
+	INSERT INTO datawarehouseho3.accounts (amount) VALUES(600000.00);
+    INSERT INTO datawarehouseho3.accounts (amount) VALUES(70000.00);
+    DELETE FROM datawarehouseho3.accounts WHERE id=1;
+    DELETE FROM datawarehouseho3.accounts WHERE id=4;
+COMMIT;
