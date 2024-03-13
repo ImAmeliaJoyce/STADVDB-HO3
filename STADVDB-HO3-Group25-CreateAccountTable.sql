8b765f68585db35c3a1b16cb40128cbf56fb4324
Amelia Abenoja <97102877+ImAmeliaJoyce@users.noreply.github.com>
2024-03-14 02:36:48 +0800

    Modified Data Warehouse

--- STADVDB-HO3-Group25-CreateAccountTable.sql
+++ STADVDB-HO3-Group25-CreateAccountTable.sql
@@ -1,5 +1,5 @@
 /*
-	This file creates the table for the transactions
+	This file creates the data warehouse for transactions
     Accounts(id, amount)
 */
 
@@ -7,8 +7,18 @@ CREATE SCHEMA `datawarehouseho3` ;
 
 CREATE TABLE `datawarehouseho3`.`accounts` (
 	id INT AUTO_INCREMENT PRIMARY KEY,
-    amount INT
+    amount DECIMAL(10,2)
 );
 
--- Check the values in the accounts table
+-- Check the values in the accounts table before inserting values
+SELECT * FROM datawarehouseho3.accounts;
+
+-- Insert initial values into table
+INSERT INTO datawarehouseho3.accounts (amount) VALUES(10.00);
+INSERT INTO datawarehouseho3.accounts (amount) VALUES(200.00);
+INSERT INTO datawarehouseho3.accounts (amount) VALUES(3000.00);
+INSERT INTO datawarehouseho3.accounts (amount) VALUES(40000.00);
+INSERT INTO datawarehouseho3.accounts (amount) VALUES(500000.00);
+
+-- Check the values in the accounts table after inserting values
 SELECT * FROM datawarehouseho3.accounts;
