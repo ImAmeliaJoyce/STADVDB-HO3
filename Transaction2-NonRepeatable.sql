5ba2062cc9d6d15779d335a05c676a5bf010fafc
Amelia Abenoja <97102877+ImAmeliaJoyce@users.noreply.github.com>
2024-03-14 03:08:38 +0800

    T2 (Read) Non-Repeatable

---
+++ Transaction2-NonRepeatable.sql
@@ -0,0 +1,27 @@
+/*
+	TRANSACTION 2
+    - Handles the Read(X) Operation
+*/
+
+-- Set Isolation Level
+SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED;
+-- SET SESSION TRANSACTION ISOLATION LEVEL READ COMMITTED;
+-- SET SESSION TRANSACTION ISOLATION LEVEL REPEATABLE READ;
+-- SET SESSION TRANSACTION ISOLATION LEVEL SERILIZABLE;
+
+
+-- Scenario 2 (Non-repeatable Read)
+-- FLOW: Execute this query from Transaction 2 first,
+-- while DO SLEEP(10) is executing, run corresponding query in Transaction 1
+
+START TRANSACTION;
+	SELECT amount 
+    FROM datawarehouseho3.accounts
+    WHERE id = 3;
+    
+    DO SLEEP(10);
+    
+    SELECT amount 
+    FROM datawarehouseho3.accounts
+    WHERE id = 3;
+COMMIT;
