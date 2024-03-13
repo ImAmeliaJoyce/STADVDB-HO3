2735268401633b84e63ff0d120e5b49c94343e4c
Amelia Abenoja <97102877+ImAmeliaJoyce@users.noreply.github.com>
2024-03-14 03:21:24 +0800

    T1 (Write) Summary

--- STADVDB-HO3-Group25-Transaction1.sql
+++ STADVDB-HO3-Group25-Transaction1.sql
@@ -1,41 +1,43 @@
 /*
 	TRANSACTION 1
-	This script file handles the write transactions to be done
+    - Handles the Write(X) Operation
 */
 
--- Insert initial values into table
-INSERT INTO datawarehouseho3.accounts (amount) VALUES(10);
-INSERT INTO datawarehouseho3.accounts (amount) VALUES(200);
-INSERT INTO datawarehouseho3.accounts (amount) VALUES(3000);
-INSERT INTO datawarehouseho3.accounts (amount) VALUES(40000);
-INSERT INTO datawarehouseho3.accounts (amount) VALUES(500000);
+-- Set Isolation Level
+SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED;
+-- SET SESSION TRANSACTION ISOLATION LEVEL READ COMMITTED;
+-- SET SESSION TRANSACTION ISOLATION LEVEL REPEATABLE READ;
+-- SET SESSION TRANSACTION ISOLATION LEVEL SERILIZABLE;
 
 
+-- -------------------------------------
 -- Scenario 1 (Dirty Read)
--- FLOW: Execute this, while DO SLEEP(10) is running, execute
--- corresponding query in Transaction 2
+-- FLOW: Execute the T1 transaction until DO SLEEP, while DO SLEEP is running,
+-- go to T2 transaction and execute the corresponding query.
 START TRANSACTION;
 	UPDATE datawarehouseho3.accounts 
-    SET amount = 2 * amount
+    SET amount = (0.04 * amount) + amount
     WHERE id = 5;
 	DO SLEEP(10);
 COMMIT;
 
+
+-- -------------------------------------
 -- Scenario 2 (Non-repeatable Read)
 -- FLOW: Execute this, while DO SLEEP(10) in Transaction 2 is running
 START TRANSACTION;
 	UPDATE datawarehouseho3.accounts 
-    SET amount = 2 * amount
-    WHERE id = 5;
+    SET amount = 2.0 * amount
+    WHERE id = 3;
 COMMIT;
 
+
+-- -------------------------------------
 -- Scenario 3 (Phantom Read)
--- FLOW: Execute this, while DO SLEEP(10) is running, execute
--- corresponding query in Transaction 2
+-- FLOW: Execute this while DO SLEEP(10) is running in Transaction 2
 START TRANSACTION;
-	INSERT INTO datawarehouseho3.accounts (amount) VALUES(600000);
-    INSERT INTO datawarehouseho3.accounts (amount) VALUES(70000);
-    INSERT INTO datawarehouseho3.accounts (amount) VALUES(8000);
-    INSERT INTO datawarehouseho3.accounts (amount) VALUES(900);
+	INSERT INTO datawarehouseho3.accounts (amount) VALUES(600000.00);
+    INSERT INTO datawarehouseho3.accounts (amount) VALUES(70000.00);
+    DELETE FROM datawarehouseho3.accounts WHERE id=1;
+    DELETE FROM datawarehouseho3.accounts WHERE id=4;
 COMMIT;
-    
