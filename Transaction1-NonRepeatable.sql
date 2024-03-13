fe2a1868ff1646b1bdf949d2f51b6d213a9c6c3f
Amelia Abenoja <97102877+ImAmeliaJoyce@users.noreply.github.com>
2024-03-14 03:09:09 +0800

    T1 (Write) Non-Repeatable

---
+++ Transaction1-NonRepeatable.sql
@@ -0,0 +1,19 @@
+/*
+	TRANSACTION 1
+    - Handles the Write(X) Operation
+*/
+
+-- Set Isolation Level
+SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED;
+-- SET SESSION TRANSACTION ISOLATION LEVEL READ COMMITTED;
+-- SET SESSION TRANSACTION ISOLATION LEVEL REPEATABLE READ;
+-- SET SESSION TRANSACTION ISOLATION LEVEL SERILIZABLE;
+
+
+-- Scenario 2 (Non-repeatable Read)
+-- FLOW: Execute this, while DO SLEEP(10) in Transaction 2 is running
+START TRANSACTION;
+	UPDATE datawarehouseho3.accounts 
+    SET amount = 2.0 * amount
+    WHERE id = 3;
+COMMIT;
