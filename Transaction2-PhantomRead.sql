d99d91e11e57990f8817bd16c0a99ccc3b282645
Amelia Abenoja <97102877+ImAmeliaJoyce@users.noreply.github.com>
2024-03-14 03:20:03 +0800

    T2 (Read) PhantomRead

---
+++ Transaction2-PhantomRead.sql
@@ -0,0 +1,28 @@
+/*
+	TRANSACTION 2
+    - Handles the Read(X) Operation
+*/
+
+-- Set Isolation Level
+SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED;
+-- SET SESSION TRANSACTION ISOLATION LEVEL READ COMMITTED;
+-- SET SESSION TRANSACTION ISOLATION LEVEL REPEATABLE READ;
+-- SET SESSION TRANSACTION ISOLATION LEVEL SERILIZABLE;
+
+
+-- -------------------------------------
+-- Scenario 3 (Phantom Read)
+-- FLOW: Execute first read, then while waiting, execute corresponding
+-- query in Transaction 1, next query and commit will then be executed
+
+START TRANSACTION;
+	SELECT *
+    FROM datawarehouseho3.accounts
+    WHERE id BETWEEN 1 and 10;
+    
+    DO SLEEP(10);
+    
+	SELECT *
+    FROM datawarehouseho3.accounts
+    WHERE id BETWEEN 1 and 10;
+COMMIT;
